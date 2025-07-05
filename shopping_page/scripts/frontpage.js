d927fa1dadbcc7263441ce1168196e5b68814bca
khafu19768 <106819634+khafu19768@users.noreply.github.com>
2025-07-06 00:07:38 +0700

    Bai lam cua ca 2 dua

---
+++ shopping_page/scripts/frontpage.js
@@ -0,0 +1,8 @@
+// const facebookButton = document.querySelector('.facebook-button');
+
+// facebookButton.addEventListener('mouseenter', function() {
+//     facebookButton.innerHTML += '<i class="fab fa-facebook-f"></i> Facebook';
+// });
+// facebookButton.addEventListener('mouseleave', function() {
+//     facebookButton.innerHTML = '<img src="icon/facebook.webp" class="icon-image">';
+// });
