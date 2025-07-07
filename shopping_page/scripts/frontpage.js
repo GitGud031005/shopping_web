4532b78f714c75c8106f9e092c36dff5871385f9
khafu19768 <106819634+khafu19768@users.noreply.github.com>
2025-07-07 19:40:47 +0700

    latest version 07/07/25 19:40

--- shopping_page/scripts/frontpage.js
+++
@@ -1,8 +0,0 @@
-// const facebookButton = document.querySelector('.facebook-button');
-
-// facebookButton.addEventListener('mouseenter', function() {
-//     facebookButton.innerHTML += '<i class="fab fa-facebook-f"></i> Facebook';
-// });
-// facebookButton.addEventListener('mouseleave', function() {
-//     facebookButton.innerHTML = '<img src="icon/facebook.webp" class="icon-image">';
-// });
