a772ac30c57230d8043697f9a93a77698312c5a9
Nguyen Nhat Nam <n.nhat.nam12082005@gmail.com>
2025-07-08 15:03:13 +0700

    game nitendo

--- scripts/nitendo2_game.js
+++ scripts/nitendo2_game.js
@@ -37,7 +37,7 @@ export function RenderNitendo2Game(){
 
 RenderNitendo2Game();
 
-  document.querySelectorAll('.nitendo2-game-product-img')
+  document.querySelectorAll('.nitendo2-game-product-img-frame')
     .forEach(productImg => {
       productImg.addEventListener('mouseenter', () => {
         const frame = productImg.closest('.nitendo2-game-product-img-frame');
@@ -46,8 +46,16 @@ RenderNitendo2Game();
           const buttonDiv = document.createElement('div');
           buttonDiv.className = 'nitendo2-game-product-img-frame-order';
           buttonDiv.innerHTML = `
-            <button>h</button>
-            <button>h</button>
+          <div class="icon-nitendo2">
+            <button class="nitendo2-game-product-img-frame-button shopping">
+              <img style="width: 30px" class="nitendo2-game-product-img-frame-button-icon" src="icon/online-shopping.png">
+            </button>
+          </div>
+          <div class="icon-nitendo2">
+            <button class="nitendo2-game-product-img-frame-button search">
+              <img class="nitendo2-game-product-img-frame-button-icon" src="icon/search-512.webp">
+            </button>
+            </div>
           `;
           frame.appendChild(buttonDiv);
         }
@@ -59,3 +67,19 @@ RenderNitendo2Game();
         if (existing) existing.remove();
       });
     });
+
+    document.querySelectorAll('.nitendo2-game-product-img-frame-button.search')
+      .forEach(button => {
+        button.addEventListener('mouseenter', () => {
+          const bigDiv = button.closest('.icon-nitendo2');
+
+          if (!bigDiv.querySelector(".noti-shopping")) {
+            const notiDiv = document.createElement('div');
+            notiDiv.className = 'nitendo2-game-noti-shopping';
+            notiDiv.innerHTML = `
+              <p class="noti-shopping">Lựa chọn tuỳ chọn</p>
+            `;
+            bigDiv.prepend(notiDiv);
+          }
+        });
+      });
